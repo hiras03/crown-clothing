5880b6a9ea9562ec1f4351ccf95f178684a4baa2
Hiras <hirasharis03@gmail.com>
2019-11-30 11:46:19 +0530

    user reducer and user actions added to app component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -35,4 +35,4 @@ const Header = ({currentUser}) => (
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser
 });
-export default connect()(Header);
+export default connect(mapStateToProps)(Header);
